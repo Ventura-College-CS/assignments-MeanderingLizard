c0caddbea2ac323c971cd996e3109e8f9944d951
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-09 14:27:34 -0800

    Commit

--- t.cpp
+++ t.cpp
@@ -1,8 +1,17 @@
-main
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+    double *ptr;
 
-    
+    ptr = numbers;
+
+    cout << "\nPrinted array with pointer arithmetic: \n";
+    for (int i=0; i<N; i++)
+        cout << *(ptr+i) << "\t"; //prints by memory address, incremented by i*sizeof(double). The arithmetic expression pointing to the ith element is *(ptr+i)
+    cout << endl << "Address of first element (using ptr): " << ptr << endl; //ptr has the memory address of numbers (and the first element)
+    cout << "Address of numbers: " << &numbers << endl; //to show they are the same
+    return 0;
 }
-//something
