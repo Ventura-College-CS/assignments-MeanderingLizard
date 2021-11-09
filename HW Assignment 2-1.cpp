fe20ad6ce818eb52f215cdb92d382ff0a9f311bf
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-09 15:18:55 -0800

    Print elements in "numbers"

---
+++ HW Assignment 2-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main ()
+{
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+    double ptr = numbers[N];
+
+    cout << ptr << endl; //prints numbers[N]
+
+    /* print all elements in the array "numbers" using  the pointer *ptr*. */
+
+
+}
