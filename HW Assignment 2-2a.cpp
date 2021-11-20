717e79c5e46c4d3ce17a37bae07b13f581ee60ca
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-19 19:21:11 -0800

    Assignment 2-2 to my understanding

---
+++ HW Assignment 2-2a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+const int N = 6;
+double n [N] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+
+int main()
+{
+
+    void printArray(double * const  n, int N);
+    
+}
