d593437a210e2c1f8fd2bc1c9787d652cbde677f
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 12:27:29 -0800

    Midterm Q1

---
+++ Midterm/Midterm Q1.cpp
@@ -0,0 +1,12 @@
+// Elaborate the following:
+queue<int> q; //Queue is not an acceptable template
+
+	for (int i = 0; i < 5; i++) //there is no declaraiton
+		q.push(i);
+	cout << "The size of q " << q.size() << endl;
+
+	cout << " Dequeued : " << q.front() << endl;
+	q.pop();
+	cout << " Dequeued : " << q.front() << endl;
+	q.pop();
+//this program appears to set up a set of values, then remove the one that is first in the queue twice
