56ceacaac12da98fb2a757bc2851def85bd26717
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 20:49:39 -0800

    Q5 Start

--- Midterm/Midterm Q4.cpp
+++ Midterm/Midterm Q4.cpp
@@ -1,4 +1,4 @@
-        list<int> list1(47, 203); //list and queue aren't considered templates
+    list<int> list1(47, 203); //list and queue aren't considered templates
 	queue<int, list<int> > lq(list1);
 	cout << " Dequeued : " << lq.front() << endl;
 	lq.pop();
---
+++ Midterm/Midterm Q5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+class Student
+
+ SID: int
+ sname: string
+ scores: [0,1,2]
+
+Student();
+Student(int i, string name, double *sc);
+getID(): int;
+getName(): string;
+getScores(): double *; 
