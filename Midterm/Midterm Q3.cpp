295eb999315e2828da2a050210647c8396aec867
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 19:04:47 -0800

    Q3

--- Midterm/Midterm Q3.cpp
+++ Midterm/Midterm Q3.cpp
@@ -1,5 +1,5 @@
 int numbers[] = {17, 32, 54, 76, 93};
-	deque<int> dq(numbers, numbers + 3);
+	deque<int> dq(numbers, numbers + 3); //A Double Ended Queue (deque) allows values to be inserted at both ends
 	cout << " Dequeued : " << dq.front() << endl;
 	dq.pop_front();
 	cout << " Dequeued : " << dq.front() << endl;
