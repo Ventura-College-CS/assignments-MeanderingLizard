6c8f817c9cda2894dca7422e8b67578cc0723c84
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 19:02:01 -0800

    Q3 Progress

--- Midterm/Midterm Q3.cpp
+++ Midterm/Midterm Q3.cpp
@@ -1,4 +1,4 @@
-int numbers[] = {10, 20, 30, 40, 50};
+int numbers[] = {17, 32, 54, 76, 93};
 	deque<int> dq(numbers, numbers + 3);
 	cout << " Dequeued : " << dq.front() << endl;
 	dq.pop_front();
@@ -8,3 +8,4 @@ int numbers[] = {10, 20, 30, 40, 50};
 	dq.pop_back();
 	cout << " Dequeued from rear: " << dq.back() << endl;
 	dq.pop_back();
+	//deque is not considered a template for my VS
