11e28f07fd8b3a49e9490fbdab6d7726df7829ab
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 21:32:20 -0800

    Progress on Q5

--- Midterm/Midterm Q5.cpp
+++ Midterm/Midterm Q5.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 class Student
 
-SID: int
-sname: string
-scores: [0,1,2]
+    SID: int
+    SName: string
+    scores: [0,1,2] //should be a double array for 3 scores
 
 
-Student();
-Student(int i, string name, double *sc);
-getID(): int;
-getName(): string;
-getScores(): double *; 
+        Student();
+        Student(int i, string , double *sc);
+        getID(): int
+        getName(): string;
+        getScores(): double *
 
+//Visual seperator
 
 template <class T>
-pool: vector<T>;
-    push(el: const T &): void
-    pop(): T 
-    topEl(): T &
-    isEmpty(): bool
-    size(): int
+class stack
+    pool: vector<T>;
+        Stack();
+        push(el: const T &): void;
+        pop(): T 
+        topEl(): T &
+        isEmpty(): bool
+        size(): int
