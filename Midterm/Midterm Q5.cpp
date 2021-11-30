fac90772bdf676bddc25ed0781abd02c3acac738
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-11-29 20:55:44 -0800

    Q5 Progress 1

--- Midterm/Midterm Q5.cpp
+++ Midterm/Midterm Q5.cpp
@@ -4,12 +4,22 @@ using namespace std;
 
 class Student
 
- SID: int
- sname: string
- scores: [0,1,2]
+SID: int
+sname: string
+scores: [0,1,2]
+
 
 Student();
 Student(int i, string name, double *sc);
 getID(): int;
 getName(): string;
 getScores(): double *; 
+
+
+template <class T>
+pool: vector<T>;
+    push(el: const T &): void
+    pop(): T 
+    topEl(): T &
+    isEmpty(): bool
+    size(): int
