51ff1712faf32090ba8d199a688cfd2b18b74fa9
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-12-13 14:02:51 -0800

    Progress4

--- Final/Final3.cpp
+++ Final/Final3.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
-#include <stack>
-#include <math.h>
-#include <vector>
-#ifndef STACK
-#define STACK
+#include "genStack.h"
 using namespace std;
 
+int isOperator(char);
+int prcedence(char);
+int i;
 
+int main()
+{
+    string infix = "ab+cd*/";
 
-#endif
+    Stack<char, 10> stack;
+
+    for (i = 0; i < infix.size(); i++)
+    {
+        if (!isOperator(infix[i]))
+        {
+            cout << infix[i] << " ";
+            continue;
+
+        }
+
+        else
+            while ((!stack.isEmpty()) && (precedence(infix[i]) <precedence(stack.topEl())))
+                cout << stack.pop() << " ";
+        stack.push(infix[i]);
+
+    }
+}
+
+int precedence(char op)
+{
+
+    switch (op)
+    {
+        case '+':
+        case '-':
+            return 5;
+
+        case '*':
+        case '/':
+            return 10;
+        default:
+            cout << "Undefined Operator \n";
+            exit(0);
+    }
+
+}
+
+int isOperator(char op)
+{
+
+    switch (op)
+    {
+
+        case '+':
+	    case '-':
+	    case '*':
+	    case '/':
+		    return 1;
+	    default:
+		    return 0;
+
+    }
+
+}
