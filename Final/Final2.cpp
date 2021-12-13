72c5ce3859ab72986cddefacf615285ac7553ad2
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-12-13 00:14:16 -0800

    progress4

--- Final/Final2.cpp
+++ Final/Final2.cpp
@@ -25,4 +25,32 @@ int partition (int L[], int a, int r)
         }
     }
 
+swap (&L[j+1], &L[r+1]);
+return (j+1);
+
+}
+
+void quickSort(int L[], int a, int r)
+{
+
+    if (a<r)
+    {
+
+        int q = partition(L, a,r);
+        quickSort(L, a, q - 1);
+        quickSort(L, q+1, r);
+
+    }
+}
+
+int main()
+{
+
+    int l[] = {9,7,3,5,6,2};
+    int n = sizeof(l)/sizeof(l[0]);
+    quickSort(l,0,n-1);
+    for (int i=0; i<n; i++)
+    cout << l[i] << " ";
+    return 0;
+
 }
