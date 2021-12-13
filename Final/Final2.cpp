779e25d08dcd03826ba92bb1535cc4d7b382e626
AntonioAlvelo <antonio_alvelo1@my.vcccd.edu>
2021-12-13 00:06:30 -0800

    progress3

--- Final/Final2.cpp
+++ Final/Final2.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void swap (int *l, int *m)
+{
+
+    int temp = *l;
+
+    *l = *m;
+    *m = temp;
+}
+
+int partition (int L[], int a, int r)
+{
+
+    int x = L[r];
+    int j = a - 1;
+
+    for (int k = a; k <= r- 1; k++)
+    {
+        if(L[j], &L[k]);
+        {
+            j++;
+            swap (&L[j], &L[k]);
+        }
+    }
+
+}
